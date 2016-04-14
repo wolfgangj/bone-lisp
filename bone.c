b2732bdec688d75d8891d06d3970bac37f781ae9
Wolfgang Jaehrling <wolfgang@conseptizer.org>
2016-04-14 21:09:09 +0200

    make several functions local

--- bone.c
+++ bone.c
@@ -42,24 +42,24 @@ typedef enum { t_cons = 0, t_sym = 1, t_uniq = 2, t_str = 3, t_reg = 4, t_sub = 
 #define BINDING_DECLARED  UNIQ(104)
 #define IN_ARGS           UNIQ(105)
 #define IN_ENV            UNIQ(106)
-bool is_nil(any x) { return x == NIL; }
-bool is(any x) { return x != BFALSE; }
-any to_bool(int x) { return x ? BTRUE : BFALSE; }
+my bool is_nil(any x) { return x == NIL; }
+my bool is(any x) { return x != BFALSE; }
+my any to_bool(int x) { return x ? BTRUE : BFALSE; }
 
 my void print(any x);
 my void generic_error(const char *msg, any x) { printf("%s: ", msg); print(x); printf("\n"); abort(); }
 my void type_error(any x, type_tag t) { generic_error("type error", x); } // FIXME: show more info
-type_tag tag_of(any x) { return x & 7; }
-bool is_tagged(any x, type_tag t) { return tag_of(x) == t; }
-void check(any x, type_tag t) { if(!is_tagged(x, t)) type_error(x, t); }
-any tag(any x, type_tag t) { return x | t; }
-any untag(any x) { return x & ~7; }
-any untag_check(any x, type_tag t) { check(x, t); return untag(x); }
+my type_tag tag_of(any x) { return x & 7; }
+my bool is_tagged(any x, type_tag t) { return tag_of(x) == t; }
+my void check(any x, type_tag t) { if(!is_tagged(x, t)) type_error(x, t); }
+my any tag(any x, type_tag t) { return x | t; }
+my any untag(any x) { return x & ~7; }
+my any untag_check(any x, type_tag t) { check(x, t); return untag(x); }
 
-bool is_num(any x) { return is_tagged(x, t_num); }
+my bool is_num(any x) { return is_tagged(x, t_num); }
 // FIXME: these assume little-endian
-int32_t any2int(any x) { check(x, t_num); return ((int32_t *) &x)[1]; }
-any int2any(int32_t n) { any r = t_num; ((int32_t *) &r)[1] = n; return r; }
+my int32_t any2int(any x) { check(x, t_num); return ((int32_t *) &x)[1]; }
+my any int2any(int32_t n) { any r = t_num; ((int32_t *) &r)[1] = n; return r; }
 
 //////////////// regions ////////////////
 
@@ -80,8 +80,8 @@ my any **fresh_blocks() { any **p = blocks_alloc(ALLOC_BLOCKS_AT_ONCE); blocks_i
 my void ensure_free_block() { if(!free_block) free_block = fresh_blocks(); }
 my any **block_new(any **next) { ensure_free_block(); any **r = free_block; free_block = (any **) r[0]; r[0] = (any *) next; return r; }
 my void reg_init(reg r, any **b) { r->current_block = b; r->allocp = (any **) &r[1]; }
-reg reg_new() { any **b = block_new(NULL); reg r = (reg) &b[1]; reg_init(r, b); return r; }
-void reg_free(reg r) { block((any *) r)[0] = (any *) free_block; free_block = r->current_block; }
+my reg reg_new() { any **b = block_new(NULL); reg r = (reg) &b[1]; reg_init(r, b); return r; }
+my void reg_free(reg r) { block((any *) r)[0] = (any *) free_block; free_block = r->current_block; }
 my void blocks_sysfree(any **b) { if(!b) return; any **next = (any **) b[0]; munmap(b, blocksize); blocks_sysfree(next); }
 my void reg_sysfree(reg r) { blocks_sysfree(r->current_block); }
 
@@ -91,11 +91,11 @@ my reg *reg_sp = reg_stack; // points to tos!
 my any **allocp, **current_block; // from currently used reg.
 my void load_reg(reg r)  { allocp = r->allocp; current_block = r->current_block; }
 my void store_reg(reg r) { r->allocp = allocp; r->current_block = current_block; }
-void reg_push(reg r) { store_reg(*reg_sp); reg_sp++; *reg_sp = r;     load_reg(*reg_sp); }
-reg reg_pop()        { store_reg(*reg_sp); reg r = *reg_sp; reg_sp--; load_reg(*reg_sp); return r; }
+my void reg_push(reg r) { store_reg(*reg_sp); reg_sp++; *reg_sp = r;     load_reg(*reg_sp); }
+my reg reg_pop()        { store_reg(*reg_sp); reg r = *reg_sp; reg_sp--; load_reg(*reg_sp); return r; }
 my void reg_permanent() { reg_push(permanent_reg); }
 
-any *reg_alloc(int n) { any *res = (any *) allocp; allocp += n;
+my any *reg_alloc(int n) { any *res = (any *) allocp; allocp += n;
   if(block((any *) allocp) == current_block) return res;
   current_block = block_new(current_block); allocp = (any **) &current_block[1]; return reg_alloc(n);
 }
@@ -103,23 +103,23 @@ any *reg_alloc(int n) { any *res = (any *) allocp; allocp += n;
 my any reg2any(reg r) { return tag((any) r, t_reg); }
 my reg any2reg(any x) { return (reg) untag_check(x, t_reg); }
 
-any copy(any x); // FIXME: to header
+my any copy(any x); // FIXME: to header
 my any copy_back(any x) { reg_push(reg_sp[-1]); any y = copy(x); reg_pop(); return y; }
 
 //////////////// conses / lists ////////////////
 
-any cons(any a, any d) { any *p = reg_alloc(2); p[0] = a; p[1] = d; return (any) p; } // no tag() needed
-any precons(any a) { any *p = reg_alloc(2); p[0] = a; return (any) p; } // for faster list construction
-any far(any x) { return ((any *) x)[0]; } // fast, no typecheck
-any fdr(any x) { return ((any *) x)[1]; } // likewise
-any car(any x) { check(x, t_cons); return far(x); }
-any cdr(any x) { check(x, t_cons); return fdr(x); }
-void set_far(any cell, any x) { ((any *) cell)[0] = x; }
-void set_fdr(any cell, any x) { ((any *) cell)[1] = x; }
+my any cons(any a, any d) { any *p = reg_alloc(2); p[0] = a; p[1] = d; return (any) p; } // no tag() needed
+my any precons(any a) { any *p = reg_alloc(2); p[0] = a; return (any) p; } // for faster list construction
+my any far(any x) { return ((any *) x)[0]; } // fast, no typecheck
+my any fdr(any x) { return ((any *) x)[1]; } // likewise
+my any car(any x) { check(x, t_cons); return far(x); }
+my any cdr(any x) { check(x, t_cons); return fdr(x); }
+my void set_far(any cell, any x) { ((any *) cell)[0] = x; }
+my void set_fdr(any cell, any x) { ((any *) cell)[1] = x; }
 
-bool is_cons(any x) { return is_tagged(x, t_cons); }
-bool is_single(any x) { return is_cons(x) && is_nil(fdr(x)); }
-any single(any x) { return cons(x, NIL); }
+my bool is_cons(any x) { return is_tagged(x, t_cons); }
+my bool is_single(any x) { return is_cons(x) && is_nil(fdr(x)); }
+my any single(any x) { return cons(x, NIL); }
 #define foreach(var, lst) for(any p_ = (lst), var; is_cons(p_) && (var = far(p_), 1); p_ = fdr(p_))
 
 my int len(any x) { int n = 0; foreach(e, x) n++; return n; }
@@ -127,8 +127,8 @@ my any assoq(any obj, any xs) { foreach(x, xs) if(car(x) == obj) return fdr(x); 
 
 //////////////// strs ////////////////
 
-any str(any chrs) { any *p = reg_alloc(1); *p = chrs; return tag((any) p, t_str); }
-any unstr(any s) { return *(any *) untag_check(s, t_str); }
+my any str(any chrs) { any *p = reg_alloc(1); *p = chrs; return tag((any) p, t_str); }
+my any unstr(any s) { return *(any *) untag_check(s, t_str); }
 my any charp2list(const char *p) { return !*p ? NIL : cons(int2any(*p), charp2list(p+1)); } // FIXME: for short strings only
 my any charp2str(const char *p) { return str(charp2list(p)); }
 my char *list2charp(any x) {
@@ -142,12 +142,12 @@ my char *list2charp(any x) {
 #define MAXLOAD 175 // Value between 0 and 255; 128 will cause an average of two probes.
 typedef struct { unsigned size, taken_slots; any *keys, *vals; any default_value; } *hash;
 
-hash hash_new(unsigned initsize, any default_val) { hash h = malloc(sizeof(*h));
+my hash hash_new(unsigned initsize, any default_val) { hash h = malloc(sizeof(*h));
   h->size = initsize; h->taken_slots = 0; h->default_value = default_val;
   h->keys = malloc(initsize*sizeof(any)); h->vals = malloc(initsize*sizeof(any));
   for(unsigned i = 0; i != initsize; i++) h->keys[i] = HASH_SLOT_UNUSED; return h;
 }
-void hash_free(hash h) { free(h->keys); free(h->vals); free(h); }
+my void hash_free(hash h) { free(h->keys); free(h->vals); free(h); }
 
 /* Find the entry in H with KEY and provide the entry number in *POS.
    Return true if there is an entry with this key already.  If there
@@ -163,31 +163,31 @@ my bool find_slot(hash h, any key, unsigned *pos) { int first_deleted = -1;
   }
 }
 
-void hash_set(hash h, any key, any val); // FIXME: to header
+my void hash_set(hash h, any key, any val); // FIXME: to header
 my bool slot_used(any x) { return x != HASH_SLOT_UNUSED && x != HASH_SLOT_DELETED; } 
 my void enlarge_table(hash h) { hash new = hash_new(h->size * 2 + 1, NIL);
   for(unsigned i = 0; i != h->size; i++) if(slot_used(h->keys[i])) hash_set(new, h->keys[i], h->vals[i]);
   free(h->keys); free(h->vals); h->size = new->size; h->keys = new->keys; h->vals = new->vals; free(new);
 }
 
-void hash_set(hash h, any key, any val) { unsigned pos;
+my void hash_set(hash h, any key, any val) { unsigned pos;
   if(!find_slot(h, key, &pos)) {  // adding a new entry
     h->taken_slots++;
     if(((h->taken_slots << 8) / h->size) > MAXLOAD) { enlarge_table(h); find_slot(h, key, &pos); }
   }
   h->keys[pos] = key; h->vals[pos] = val;
 }
-any hash_get(hash h, any key) { unsigned pos; return find_slot(h, key, &pos) ? h->vals[pos] : h->default_value; }
-void hash_rm(hash h, any key) { unsigned pos; if(find_slot(h, key, &pos)) { h->keys[pos] = HASH_SLOT_DELETED; h->taken_slots--; } }
+my any hash_get(hash h, any key) { unsigned pos; return find_slot(h, key, &pos) ? h->vals[pos] : h->default_value; }
+my void hash_rm(hash h, any key) { unsigned pos; if(find_slot(h, key, &pos)) { h->keys[pos] = HASH_SLOT_DELETED; h->taken_slots--; } }
 #if 0 // FIXME: hash_iter
-void hash_each(hash h, hash_iter fn, void *hook) {
+my void hash_each(hash h, hash_iter fn, void *hook) {
   for(unsigned i = 0; i != h->size; i++) if(slot_used(h->keys[i])) fn(hook, h->keys[i], h->vals[i]);
 }
 #endif
 
 //////////////// syms ////////////////
 
-bool is_sym(any x) { return is_tagged(x, t_sym); }
+my bool is_sym(any x) { return is_tagged(x, t_sym); }
 my hash sym_ht;
 my any string_hash(const char *s, size_t *len) {  // This is the djb2 algorithm.
   int32_t hash = 5381; *len = 0;
@@ -200,7 +200,7 @@ my any add_sym(const char *name, size_t len, any id) {
   reg_permanent(); char *new = (char *) reg_alloc(bytes2words(len+1)); reg_pop();
   memcpy(new, name, len); hash_set(sym_ht, id, (any) new); return as_sym(new);
 }
-any intern(const char *name) { size_t len; any id = string_hash(name, &len);
+my any intern(const char *name) { size_t len; any id = string_hash(name, &len);
   while(1) {
     char *candidate = (char *) hash_get(sym_ht, id);
     if(candidate == NULL) return add_sym(name, len, id);
@@ -210,7 +210,7 @@ any intern(const char *name) { size_t len; any id = string_hash(name, &len);
 }
 my any intern_from_chars(any chrs) { char *s = list2charp(chrs); any res = intern(s); free(s); return res; }
 
-any s_quote, s_quasiquote, s_unquote, s_unquote_splicing, s_lambda, s_let, s_letrec, s_dot;
+my any s_quote, s_quasiquote, s_unquote, s_unquote_splicing, s_lambda, s_let, s_letrec, s_dot;
 #define x(name) s_ ## name = intern(#name)
 my void init_syms() { x(quote);x(quasiquote);x(unquote);s_unquote_splicing=intern("unquote-splicing");
   x(lambda);x(let);x(letrec);s_dot=intern("."); }
@@ -239,7 +239,7 @@ typedef struct { sub_code code; any env[0]; } *sub;
 my bool is_sub(any x) { return is_tagged(x, t_sub); }
 my any sub2any(sub s) { return tag((any) s, t_sub); }
 my sub any2sub(any x) { return (sub) untag_check(x, t_sub); }
-any copy(any x);
+my any copy(any x);
 my any copy_sub(any x) { sub s = any2sub(x); int envsize = s->code->size_of_env;
   any *p = reg_alloc(1+envsize); *p++ = (any) s->code; for(int i = 0; i != envsize; i++) *p++ = copy(s->env[i]);
   return tag((any) p, t_sub);
@@ -299,7 +299,7 @@ my void say(any x) { switch(tag_of(x)) {
 
 //////////////// read ////////////////
 
-void parse_error(const char *text) { printf("parse error: %s\n", text); abort(); } // FIXME
+my void parse_error(const char *text) { printf("parse error: %s\n", text); abort(); } // FIXME
 
 my bool allowed_chars[] = { // these can be used for syms in s-exprs
   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -392,7 +392,7 @@ my any reader() { int c = find_token(); switch(c) {
   case EOF: return ENDOFFILE;
   default: return(chars_to_num_or_sym(read_sym_chars(c))); }
 }
-any bone_read() {
+my any bone_read() {
   any x = reader();
   if(x == READER_LIST_END) parse_error("unexpected closing parenthesis");
   return x;
@@ -428,7 +428,7 @@ my void add_rest_arg() { sub_code sc = next_call->to_be_called->code;
 my void verify_argc(struct upcoming_call *the_call) {
   if(the_call->nonrest_args_left) args_error_unspecific(the_call->to_be_called->code);
 }
-void call(sub subr, any *args) { // FIXME: move call_stack_sp++ into OP_CALL?
+my void call(sub subr, any *args) { // FIXME: move call_stack_sp++ into OP_CALL?
   call_stack_sp++; call_stack_sp->tail_calls = 0; call_stack_sp->subr = subr; sub lambda; any *lambda_envp;
 start:;
   any *env = subr->env; any *ip = subr->code->code;
@@ -479,8 +479,8 @@ my void apply(sub subr, any xs) { sub_code sc = subr->code; int argc = sc->argc,
 //////////////// bindings ////////////////
 
 my hash bindings; // FIXME: does it need mutex protection?
-void bind(any name, any subr) { reg_permanent(); hash_set(bindings, name, cons(BINDING_DEFINED, subr)); reg_pop(); } // FIXME: reg-change?
-any get_binding(any name) { return hash_get(bindings, name); }
+my void bind(any name, any subr) { reg_permanent(); hash_set(bindings, name, cons(BINDING_DEFINED, subr)); reg_pop(); } // FIXME: reg-change?
+my any get_binding(any name) { return hash_get(bindings, name); }
 
 //////////////// compiler ////////////////
 
@@ -508,31 +508,31 @@ my sub_code compile2sub_code(any e) { any raw = compile2list(e);
 
 //////////////// library ////////////////
 
-void CSUB_simpleplus(any *args) { last_value = int2any(any2int(args[0]) + any2int(args[1])); }
-void CSUB_fullplus(any *args) { int ires = 0; foreach(n, args[0]) ires += any2int(n); last_value = int2any(ires); }
-void CSUB_cons(any *args) { last_value = cons(args[0], args[1]); }
-void CSUB_print1(any *args) { print(args[0]); last_value = single(args[0]); }
-void CSUB_print(any *args) { foreach(x, args[0]) print(x); last_value = args[0]; }
-void CSUB_apply(any *args) { apply(any2sub(args[0]), args[1]); } // FIXME: (apply foo a b c xs)
-void CSUB_id(any *args) { last_value = args[0]; }
-void CSUB_nilp(any *args) { last_value = to_bool(args[0] == NIL); }
-void CSUB_eqp(any *args) { last_value = to_bool(args[0] == args[1]); }
-void CSUB_not(any *args) { last_value = to_bool(args[0] == BFALSE); }
-void CSUB_car(any *args) { last_value = car(args[0]); }
-void CSUB_cdr(any *args) { last_value = cdr(args[0]); }
-void CSUB_consp(any *args) { last_value = to_bool(is_tagged(args[0], t_cons)); }
-void CSUB_symp(any *args) { last_value = to_bool(is_tagged(args[0], t_sym)); }
-void CSUB_subp(any *args) { last_value = to_bool(is_tagged(args[0], t_sub)); }
-void CSUB_nump(any *args) { last_value = to_bool(is_tagged(args[0], t_num)); }
-void CSUB_strp(any *args) { last_value = to_bool(is_tagged(args[0], t_str)); }
-void CSUB_str(any *args) { last_value = str(args[0]); }
-void CSUB_unstr(any *args) { last_value = unstr(args[0]); }
-void CSUB_len(any *args) { last_value = int2any(len(args[0])); }
-void CSUB_assoq(any *args) { last_value = assoq(args[0], args[1]); }
-void CSUB_intern(any *args) { last_value = intern_from_chars(unstr(args[0])); }
-void CSUB_copy(any *args) { last_value = copy(args[0]); }
-void CSUB_say1(any *args) { say(args[0]); last_value = single(args[0]); }
-void CSUB_say(any *args) { foreach(x, args[0]) say(x); last_value = args[0]; }
+my void CSUB_simpleplus(any *args) { last_value = int2any(any2int(args[0]) + any2int(args[1])); }
+my void CSUB_fullplus(any *args) { int ires = 0; foreach(n, args[0]) ires += any2int(n); last_value = int2any(ires); }
+my void CSUB_cons(any *args) { last_value = cons(args[0], args[1]); }
+my void CSUB_print1(any *args) { print(args[0]); last_value = single(args[0]); }
+my void CSUB_print(any *args) { foreach(x, args[0]) print(x); last_value = args[0]; }
+my void CSUB_apply(any *args) { apply(any2sub(args[0]), args[1]); } // FIXME: (apply foo a b c xs)
+my void CSUB_id(any *args) { last_value = args[0]; }
+my void CSUB_nilp(any *args) { last_value = to_bool(args[0] == NIL); }
+my void CSUB_eqp(any *args) { last_value = to_bool(args[0] == args[1]); }
+my void CSUB_not(any *args) { last_value = to_bool(args[0] == BFALSE); }
+my void CSUB_car(any *args) { last_value = car(args[0]); }
+my void CSUB_cdr(any *args) { last_value = cdr(args[0]); }
+my void CSUB_consp(any *args) { last_value = to_bool(is_tagged(args[0], t_cons)); }
+my void CSUB_symp(any *args) { last_value = to_bool(is_tagged(args[0], t_sym)); }
+my void CSUB_subp(any *args) { last_value = to_bool(is_tagged(args[0], t_sub)); }
+my void CSUB_nump(any *args) { last_value = to_bool(is_tagged(args[0], t_num)); }
+my void CSUB_strp(any *args) { last_value = to_bool(is_tagged(args[0], t_str)); }
+my void CSUB_str(any *args) { last_value = str(args[0]); }
+my void CSUB_unstr(any *args) { last_value = unstr(args[0]); }
+my void CSUB_len(any *args) { last_value = int2any(len(args[0])); }
+my void CSUB_assoq(any *args) { last_value = assoq(args[0], args[1]); }
+my void CSUB_intern(any *args) { last_value = intern_from_chars(unstr(args[0])); }
+my void CSUB_copy(any *args) { last_value = copy(args[0]); }
+my void CSUB_say1(any *args) { say(args[0]); last_value = single(args[0]); }
+my void CSUB_say(any *args) { foreach(x, args[0]) say(x); last_value = args[0]; }
 
 my void register_csub(csub cptr, const char *name, int argc, bool has_rest) {
   any name_sym = intern(name); sub_code code = make_sub_code(name_sym, argc, has_rest, 0, 0, 2);
@@ -570,7 +570,7 @@ my void init_csubs() {
 
 //////////////// misc ////////////////
 
-any copy(any x) {
+my any copy(any x) {
   switch(tag_of(x)) {
   case t_cons: return cons(copy(far(x)), copy(fdr(x)));
   case t_str: return str(copy(unstr(x)));
