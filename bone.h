4dbe15fa2dd6d12c222640a2da9284a7e648f642
Wolfgang Jaehrling <wolfgang@conseptizer.org>
2016-07-13 19:06:03 +0200

    changed `foreach_cons` to only enter body with actual conses

--- bone.h
+++ bone.h
@@ -75,7 +75,7 @@ any single(any x);
 any list2(any a, any b);
 any list3(any a, any b, any c);
 #define foreach(var, lst) for(any p_ = (lst), var; is_cons(p_) && (var = far(p_), 1); p_ = fdr(p_))
-#define foreach_cons(var, lst) for(any var = (lst); !is_nil(var); var = fdr(var))
+#define foreach_cons(var, lst) for(any var = (lst); is_cons(var); var = fdr(var))
 
 typedef struct { any xs, last; } listgen;
 listgen listgen_new();
